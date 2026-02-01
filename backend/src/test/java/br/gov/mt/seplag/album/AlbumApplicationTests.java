ab4074e8422e2d7047bd659e68a5e4c68d20e673
CarlosdHxH <carlosdaniel.prog@gmail.com>
2026-01-31 20:21:53 -0400

    Inicialize a estrutura do projeto backend com o Maven com configuração básica do Spring Boot.

---
+++ backend/src/test/java/br/gov/mt/seplag/album/AlbumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.mt.seplag.album;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
