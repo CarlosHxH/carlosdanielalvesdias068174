8ae4686d3c9e81aa5e8a4429ee89b7c3b3f0d4af
06817435145 <carlos.dias@facilmova.com.br>
2026-02-04 13:14:33 -0400

    Atualiza Swagger

---
+++ backend/src/main/java/com/album/seplag/swagger/SwaggerConfig.java
@@ -0,0 +1,54 @@
+package com.album.seplag.swagger;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.servers.Server;
+
+@OpenAPIDefinition
+@Configuration
+@SecurityScheme(name = SwaggerConfig.SECURITY_SCHEME_NAME, type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT")
+public class SwaggerConfig {
+
+    public static final String SECURITY_SCHEME_NAME = "Bearer Authentication";
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+
+                .info(new Info()
+                        .title("Album API")
+                        .version("1.0.0")
+                        .description("API REST para gerenciamento de artistas e álbuns musicais")
+                        .contact(new Contact()
+                                .name("Carlos")
+                                .email("carlosdaniel.prog@gmail.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")))
+
+                .servers(List.of(
+                        new Server()
+                                .url("http://localhost:8080")
+                                .description("Servidor Local"),
+
+                        new Server()
+                                .url("http://backend:8080")
+                                .description("Servidor Docker")))
+
+                .components(new Components());
+    }
+}
